5eec3c8587d2e40aa478108a2372d87116847bd0
skt-t1-byungi <tiniwz@gmail.com>
2019-01-02 20:59:53 +0900

    reindent

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 module.exports = {
-  "extends": ["tslint:recommended", "tslint-config-standard"],
-  "rules": {
-    "arrow-parens": false,
-    "no-shadowed-variable": false,
-    "no-conditional-assignment": false,
-    "interface-name": false,
-    "ter-indent": [true, 4, { SwitchCase: 1 }],
-    "max-line-length": [true, 140]
-  }
+    "extends": ["tslint:recommended", "tslint-config-standard"],
+    "rules": {
+        "arrow-parens": false,
+        "no-shadowed-variable": false,
+        "no-conditional-assignment": false,
+        "interface-name": false,
+        "ter-indent": [true, 4, { SwitchCase: 1 }],
+        "max-line-length": [true, 140]
+    }
 }
